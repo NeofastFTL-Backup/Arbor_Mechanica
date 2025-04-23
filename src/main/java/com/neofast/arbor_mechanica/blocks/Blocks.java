c5673252e211774b6cbf810c9e3ffd6188f7a857
NeofastFTL <neofastftl2@gmail.com>
2025-04-23 12:44:35 +0200

    init

---
+++ src/main/java/com/neofast/arbor_mechanica/blocks/Blocks.java
@@ -0,0 +1,38 @@
+package com.neofast.arbor_mechanica.blocks;
+
+import com.neofast.arbor_mechanica.ArborMechanica;
+import com.neofast.arbor_mechanica.items.Items;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class Blocks {
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks(ArborMechanica.MOD_ID);
+
+    public static final DeferredBlock<Block> WOODEN_GEAR_BOX = registerBlock("wooden_gear_box",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(1f).sound(SoundType.GRASS)));
+
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        Items.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/com/neofast/arbor_mechanica/items/Items.java
@@ -0,0 +1,27 @@
+package com.neofast.arbor_mechanica.items;
+
+import com.neofast.arbor_mechanica.ArborMechanica;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.*;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
+
+public class Items {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ArborMechanica.MOD_ID);
+
+    public static final DeferredItem<Item> WOODEN_GEAR = ITEMS.register("wooden_gear",
+            () -> new Item(new Item.Properties()) {
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.arbor_mechanica.wooden_gear.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
